f607c6571f86afa53473b6ff0baf01a86073f4e2
Zainab Ayaz <qayaz9465@gmail.com>
2025-02-17 11:11:09 +0500

    new changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Head from "next/head";
 import Link from "next/link";
 import './globals.css';
+import { Metadata } from "next"; 
 
 export const metadata: Metadata = {
   title: "FKWC",
